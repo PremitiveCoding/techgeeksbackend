8f632e50ce1f34d3b311a21fd1adc816b64e8f06
PremitiveCoding <58826270+PremitiveCoding@users.noreply.github.com>
2022-09-09 17:25:16 +0200

    geek

---
+++ passport.js
@@ -0,0 +1,60 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GithubStrategy = require("passport-github2").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const passport = require("passport");
+
+const GOOGLE_CLIENT_ID ="1074024476236-525jervf14aimahao0pqnetf7dshvm1h.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
+
+GITHUB_CLIENT_ID = "c42b77639a7e4df1801c";
+GITHUB_CLIENT_SECRET = "36c05a27056bac991828daba3b8506245b300831";
+
+FACEBOOK_APP_ID = "801109010924017";
+FACEBOOK_APP_SECRET = "your id";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: "/auth/facebook/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
