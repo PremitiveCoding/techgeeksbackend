8425681870035c1fc036b98f1ab8f650bb0e77d9
PremitiveCoding <58826270+PremitiveCoding@users.noreply.github.com>
2022-09-09 17:49:18 +0200

    geek

--- passport.js
+++ passport.js
@@ -3,15 +3,6 @@ const GithubStrategy = require("passport-github2").Strategy;
 const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require("passport");
 
-const GOOGLE_CLIENT_ID ="1074024476236-525jervf14aimahao0pqnetf7dshvm1h.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
-
-GITHUB_CLIENT_ID = "c42b77639a7e4df1801c";
-GITHUB_CLIENT_SECRET = "36c05a27056bac991828daba3b8506245b300831";
-
-FACEBOOK_APP_ID = "801109010924017";
-FACEBOOK_APP_SECRET = "your id";
-
 passport.use(
   new GoogleStrategy(
     {
